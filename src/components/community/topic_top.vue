82b811683ae6cb92f96f28d6acc9fe0f32ca1f34
糖醋虾仁 <liuchuana@hotmail.com>
2024-06-29 13:39:55 +0800

    perf 展示时间修改为创建时间

--- src/components/community/topic_top.vue
+++ src/components/community/topic_top.vue
@@ -10,8 +10,8 @@
         >
             <div class="m-topic-top">
                 <div class="m-topic-top__time">
-                    <i :class="getTimeAgo(data.updated_at).icon"></i>
-                    <span>{{ getTimeAgo(data.updated_at).text }} </span>
+                    <i :class="getTimeAgo(data.created_at).icon"></i>
+                    <span>{{ getTimeAgo(data.created_at).text }} </span>
                 </div>
                 <div class="m-topic-top_right">
                     <div class="u-item">
